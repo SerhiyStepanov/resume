5c06b841e373170d9614b052964ba1a1f069d8a9
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-03-20 21:28:07 +0200

    add padding image

--- src/components/photo/Photo.js
+++ src/components/photo/Photo.js
@@ -4,7 +4,7 @@ import s from "./photo.module.css";
 export default function Photo() {
   return (
     <div className={s.box}>
-      <img src={photo} alt="Serhiy" className={s.photo} />
+      <img src={photo} alt="avatar" className={s.photo} />
     </div>
   );
 }
